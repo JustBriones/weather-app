44e4fffe4c06d903587e3c1fa99e2ad58129f31c
Justin Briones <justbriones@gmail.com>
2017-06-04 21:21:03 -0700

    advance promise concepts explored in promise-2.js file

--- playground/promise.js
+++ playground/promise.js
@@ -11,11 +11,14 @@ var asyncAdd = (a, b) => {
   });
 }
 
-asyncAdd(5, 7).then((res) => {
+asyncAdd(5, '7').then((res) => {
   console.log('Result: ', res);
-}, (errorMessage) => {
+  return asyncAdd(res, 33);
+}).then((res) => {
+  console.log('should be 45', res);
+}).catch((errorMessage) => {
   console.log(errorMessage);
-});
+})
 //
 //
 //
